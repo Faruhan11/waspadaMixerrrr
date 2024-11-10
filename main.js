989d67e8633cc2ebfe8a600e1267678d182eb549
Faruhan11 <137032809+Faruhan11@users.noreply.github.com>
2024-11-10 14:55:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,204 @@
+let students_M = ["   fay  ", "  apdal ", "  rehan ", " ripki  ", "  nadip ", "  bany  ", "  adit  ", "himendra", "  irsat ", " basmi  ", "om nanda", " dipan  ", " farhan ", "  aldi  "];
+let students_F = ["   al   ", "sipapasi", "   eca  ", "  ibu   ", " wilda  ", "  feli  ", "   el   ", "  kiran ", " tazkia ", " devni  ", "  alika ", "  neta  ", "  riri  ", "  cella ", " sofia  ", "  aura  ", " yuyuayu", "   cia  ", "  josu  ", "   dea  ", "  nindi ", "  andin "];
+let chmate = Array.from({ length: 18 }, () => Array(2).fill(""));
+
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
+let k = 609;
+let intervalId; 
+let clickSound = document.getElementById("clickSound");
+let backgroundSound = document.getElementById("backgroundSound"); 
+
+function updateText() {
+    shuffle(students_F);
+    shuffle(students_M);
+    let i = 0;
+    for (let m = 0; m < 14; m++, i++) {
+        chmate[i][0] = students_M[m];
+        chmate[i][1] = students_M[++m];
+    }
+    for (let f = 0; f < 22; f++, i++) {
+        chmate[i][0] = students_F[f];
+        chmate[i][1] = students_F[++f];
+    }
+    shuffle(chmate);
+
+    for (let i = 0; i < 18; i++) {
+        for (let j = 0; j < 2; j++) {
+            document.getElementById(`${i}${j}`).innerHTML = chmate[i][j];
+        }
+    }
+
+    k--;
+    if (k <= 0) {
+        clearInterval(intervalId); // Stop the loop when k reaches 0
+    }
+    if (backgroundSound.paused) {
+        backgroundSound.play();
+    }
+}
+// Function to stop the loop
+function stopLoop() {
+
+    clearInterval(intervalId);
+    backgroundSound.pause();
+    backgroundSound.currentTime = 0;
+    clickSound.play()
+
+}
+let timer;
+// Attach the event listener to the document
+document.addEventListener('click', stopLoop);
+document.addEventListener("touchstart", function() {
+                timer =setTimeout(function() {
+                    for(let i = 0; i < 18; i++){
+
+                        for(let j = 0; j < 2; j++){
+                            document.getElementById('${i}${j}').innerHTML = haha[i][j];
+                        }
+                    }
+                    stopLoop();
+                
+                 }, 2000); // 2000 milliseconds = 2 seconds
+            });
+
+
+// Start the loop, updating every 1 second (1000 milliseconds)
+intervalId = setInterval(updateText, 150);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tadinya mah mau bikin buat acak kelompok + individu, tapi males bjir jadi 2 button di
+//homepage bakal "coming soon" untuk selamanya
+
+//semoga ga duduk di depan, boleh si di depan asal yang pojok, di tengah mah diajak podcast sama pa eman anying
+//semoga yg sebangku bawa buku paket
+//semoga masuk itb
+
+
+//emang doa ngaruh?
+
+
+//idk but the hope itself is pretty like in me (the) dna
+
+
+
+//whoever found this easter egg just keep it yourself bruhh
